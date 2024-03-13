e6add776dd8ff20e945dd30cafe3cba9767d4cf4
Venkatesh <125721129+VenketeswarSahoo@users.noreply.github.com>
2024-03-13 15:57:39 +0530

    Create Breadcrumbs.jsx

---
+++ Breadcrumbs/Breadcrumbs.jsx
@@ -0,0 +1,31 @@
+// Breadcrumbs.js
+
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+const Breadcrumbs = () => {
+  const location = useLocation();
+  const pathnames = location.pathname.split("/").filter(x => x);
+
+  return (
+    <div>
+      <Link to="/" className="text-sm text-zinc-500">
+        Home{"/"}
+      </Link>
+      {pathnames.map((name, index) => {
+        const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
+        const isLast = index === pathnames.length - 1;
+
+        return isLast
+          ? <span key={name} className="text-sm text-zinc-500">
+              {name}
+            </span>
+          : <span key={name} className="text-sm text-zinc-500">
+              <Link to={routeTo}>{name}</Link> {"/"}
+            </span>;
+      })}
+    </div>
+  );
+};
+
+export default Breadcrumbs;

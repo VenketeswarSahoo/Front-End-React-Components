3dc83bd8a3fd308e22e00822a1a541881cd42d44
Venkatesh <125721129+VenketeswarSahoo@users.noreply.github.com>
2024-03-13 16:24:50 +0530

    Create App.js

---
+++ Scrolling Animation/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Section from "./Section";
+
+const App = () => {
+  return (
+    <div>
+      <Section>Animation</Section>
+      <Section>when</Section>
+      <Section>in</Section>
+      <Section>view!</Section>
+    </div>
+  );
+};
+
+export default App;
